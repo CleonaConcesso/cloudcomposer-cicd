473a6950bcb392227cdde518e36ad00481643403
CleonaConcesso <69181903+CleonaConcesso@users.noreply.github.com>
2024-10-06 22:30:22 +0530

    Update stock_data_dag_test.py

--- dags/stock_data_dag_test.py
+++ dags/stock_data_dag_test.py
@@ -2,5 +2,5 @@ from internal_unit_testing import assert_has_valid_dag
 from stock_data_dag import stock_data_dag  # Import the specific DAG from stock_data_dag.py
 
 def test_dag_import():
-    assert_has_valid_dag(stock_data_dag)   
+    assert_has_valid_dag(stock_data_dag.stock_data_dag)   
 
