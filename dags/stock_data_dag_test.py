bb8823d1ac958826ff9e551f3b4c17cd4711c615
CleonaConcesso <69181903+CleonaConcesso@users.noreply.github.com>
2024-10-07 10:02:20 +0530

    Update stock_data_dag_test.py

--- dags/stock_data_dag_test.py
+++ dags/stock_data_dag_test.py
@@ -3,12 +3,6 @@ import internal_unit_testing
 
 
 def test_dag_import():
-    """Test that the DAG file can be successfully imported.
-
-    This tests that the DAG can be parsed, but does not run it in an Airflow
-    environment. This is a recommended confidence check by the official Airflow
-    docs: https://airflow.incubator.apache.org/tutorial.html#testing
-    """
     from . import stock_data_dag as module
 
     internal_unit_testing.assert_has_valid_dag(module)
