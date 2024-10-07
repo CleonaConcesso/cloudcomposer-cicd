eef883968bceef879dd0485a15ba8abfd7b09790
CleonaConcesso <69181903+CleonaConcesso@users.noreply.github.com>
2024-10-07 06:55:50 +0530

    Update stock_data_dag_test.py

--- dags/stock_data_dag_test.py
+++ dags/stock_data_dag_test.py
@@ -1,6 +1,6 @@
-# import internal_unit_testing
+import internal_unit_testing
 
 
-# def test_dag_import():
-#     from . import stock_data_dag as module
-#    # internal_unit_testing.assert_has_valid_dag(module)  # Pass the DAG object to the testing function
+def test_dag_import():
+   from . import stock_data_dag as module
+   internal_unit_testing.assert_has_valid_dag(module)  # Pass the DAG object to the testing function
